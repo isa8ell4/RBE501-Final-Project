23b1c4faac7078f08d1bed2e54802c2a9653cfb0
isa8ell4 <alrosenstein@wpi.edu>
2024-12-13 16:34:49 -0500

    added wrench photos

--- OpenManip_Skeleton-main/src/currentTorqueRelationship.m
+++ OpenManip_Skeleton-main/src/currentTorqueRelationship.m
@@ -3,9 +3,9 @@ function [coefficients]=currentTorqueRelationship(avgCurrent,torques)
 %   Detailed explanation goes here
 
 % Scatter plot
-figure;
-scatter(avgCurrent, torques, 'filled'); % Scatter plot with filled markers
-hold on;
+% figure;
+% scatter(avgCurrent, torques, 'filled'); % Scatter plot with filled markers
+% hold on;
 
 % Line of best fit (linear regression)
 coefficients = polyfit(avgCurrent, torques, 1);% First-order polynomial fit
@@ -13,19 +13,19 @@ line_x = linspace(min(avgCurrent), max(avgCurrent), 100); % Generate x-values fo
 line_y = polyval(coefficients, line_x); % Compute y-values for the line
 
 % Plot the line of best fit
-plot(line_x, line_y, 'r-', 'LineWidth', 1.5);
-
-% Display the equation of the line
-equation = sprintf('y = %.3fx + %.3f', coefficients(1), coefficients(2));
-text(mean(avgCurrent), mean(torques), equation, 'FontSize', 12, 'Color', 'blue', 'HorizontalAlignment', 'left', 'VerticalAlignment', 'top');
-
-% Labeling the plot
-xlabel('Current(A)');
-ylabel('Torque (Nm)');
-title('Current-Torque Relationship at Joints 1-4');
-legend('Data points', 'Best fit line');
-grid on;
-
-hold off;
+% plot(line_x, line_y, 'r-', 'LineWidth', 1.5);
+% 
+% % Display the equation of the line
+% equation = sprintf('y = %.3fx + %.3f', coefficients(1), coefficients(2));
+% text(mean(avgCurrent), mean(torques), equation, 'FontSize', 12, 'Color', 'blue', 'HorizontalAlignment', 'left', 'VerticalAlignment', 'top');
+% 
+% % Labeling the plot
+% xlabel('Current(A)');
+% ylabel('Torque (Nm)');
+% title('Current-Torque Relationship at Joints 1-4');
+% legend('Data points', 'Best fit line');
+% grid on;
+% 
+% hold off;
 end
 
--- OpenManip_Skeleton-main/src/lab1_base.m
+++ OpenManip_Skeleton-main/src/lab1_base.m
@@ -1,5 +1,5 @@
 %% Setup robot
-speedRate = 1; % Constant that determines speed percentage
+speedRate = 0.6; % Constant that determines speed percentage
 travelTime = 1 / speedRate; % Defines the travel time (seconds)
 acc_time = 0.5 / speedRate; % Acceleration time (seconds)
 robot = Robot(); % Creates robot object
@@ -22,10 +22,12 @@ currents = [1000, 1000, 1000, 1000];
 % [allCurrentT3AB] = lspbVelocityControlAB(robot, thetasA, thetasB, thetasC, travelTime, speedRate);
 % avgAllCurrT3 = [mean(allCurrentT3AB(:,1)), mean(allCurrentT3AB(:,2)), mean(allCurrentT3AB(:,3)), mean(allCurrentT3AB(:,4))];
 
+%% 
 
-% % % Task 4
+% % Task 4
 % clc;
 % HW4 stuff
+
 ws = [[0,0,1];
       [0,1,0];
       [0,1,0];
@@ -35,11 +37,12 @@ l1 = 0.096326;
 l2z = 0.128;
 l2x = 0.024;
 l3 = 0.12074;
-l4 = 145.45;
+l4 = 0.14545;
 slist = [[0; 0; 1; 0;            0; 0     ]...
          [0; 1; 0; -(l1);     0; 0     ]...
          [0; 1; 0; -(l1+l2z); 0; l2x   ]...
          [0; 1; 0; -(l1+l2z); 0; l2x+l3]];
+
 
 M = [0,0,-1,l2x+l3+l4;
      0,1,0,0;
@@ -101,100 +104,117 @@ tauAppliedID = InverseDynamics(deg2rad(thetasA), dthetalist, ddthetalist, g, Fti
 
 robot.writeMode('cp')
 robot.writeCurrents(currents);
-% robot.writeJoints(thetasA); % Write joints to zero position
-pause(5);
-
+robot.writeJoints(thetasA); % Write joints to zero position
+pause(1);
+b1 = cross([-1; 0; 0], [-(l2z);0;(l4+l3+l2x)]);
+% b2 = cross([0;-1;0], [-(l2z);0;(l4+l3+l2x)])
+% b3 = cross([0;-1;0], [0;0;(l4+l3)])
+% b4 = cross([0;-1;0], [0;0;(l3)])
+bList = [[1; 0; 0; 0; 0.2902;0]...
+         [0; 1; 0; -0.2902; 0; -0.1280]...
+         [0; 1; 0;  -0.2662; 0; 0]...
+         [0; 1; 0; -0.1207; 0; 0]];
 %%% static readings at pos A
-% currReadings = zeros(1,4);
-% tic;
-% while toc < 20
-%     readings = robot.getJointsReadings();
-%     currReadings = vertcat(currReadings, readings(3, :));
-% end
-% currReadings = currReadings(2:end, :);
-% avgCurrAppliedW = [mean(currReadings(:,1)), mean(currReadings(:,2)), mean(currReadings(:,3)), mean(currReadings(:,4))];
-% 
-% tauAppliedReadings = coefficients(1).*(avgCurrAppliedW) + coefficients(2);
+currReadings = zeros(1,4);
+Jb = JacobianBody(bList, deg2rad(thetasA));
+wrenchHistory = zeros(1,4);
+tic;
+while toc < 15
+    readings = robot.getJointsReadings();
+    currReadings = vertcat(currReadings, readings(3, :));
+    tauReadings = coefficients(1,1).*(readings(3, :)*0.001) + coefficients(1,2);
+%     pause(0.1)
+%     wrench = -pinv(Jb.') * tauReadings.';
+%     wrenchHistory = vertcat(wrenchHistory, wrench);
+%     wrench = (Jb.').*(tauReadings)
+    
+end
+currReadings = currReadings(2:end, :);
+avgCurrAppliedW = [mean(currReadings(:,1)), mean(currReadings(:,2)), mean(currReadings(:,3)), mean(currReadings(:,4))];
+
+tauAppliedReadings = coefficients(1).*(avgCurrAppliedW)*0.001 + coefficients(2);
+wrenchApplied = -pinv(Jb.') * tauAppliedReadings.'
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% robot goes A to B and graph calculated torques from lspb/MR and another
-% from current readings and equation
-tic;
-% [allCurrentT1] = lspbVelocityControlAB(robot, thetasA, thetasB, thetasC, travelTime, speedRate);
-[j2current, allCurrentT1] = cpControl(robot, travelTime, thetasA, thetasB, currents, speedRate);
-% avgAllCurrT1 = [mean(allCurrentT1(:,1)), mean(allCurrentT1(:,2)), mean(allCurrentT1(:,3)), mean(allCurrentT1(:,4))];
-totalTime = toc;
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% current graph
-rowsCR = size(allCurrentT1, 1);
-timeXread = zeros(rowsCR, 1);
-currTime = 0;
-interval = totalTime/rowsCR;
-for i=1:rowsCR
-    timeXread(i) = currTime;
-    currTime = currTime + interval;
-end
-
-figure;
-xlabel('Time (s)');
-ylabel('Torque (Nm)');
-%     title('Current at Joint 2 Velocity Control')
-% title(sprintf('Motor Current at Joint 2 at %.0f%% Speed (Velocity Control)', percentSpeed*100));
-allTausReadings = zeros(1,4);
-for i=1:size(allCurrentT1,1)
-    instTauR = coefficients(1).*allCurrentT1(i,:) + coefficients(2)*ones(1,4);
-    allTausReadings = vertcat(allTausReadings, instTauR);
-end
-allTausReadings = allTausReadings(2:end, :);
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-m = 100; % number of points
-qAB1 = lspb(thetasA(1), thetasB(1), m);
-qAB2 = lspb(thetasA(2), thetasB(2), m);
-qAB3 = lspb(thetasA(3), thetasB(3), m);
-qAB4 = lspb(thetasA(4), thetasB(4), m);
-
-qAB = [qAB1,qAB2,qAB3,qAB4];
-
-allTausCalc = zeros(1,4);
-for i=1:size(qAB,1)
-    instTauCalc = 1000.*InverseDynamics(deg2rad(qAB(i,:).'), dthetalist, ddthetalist, g, Ftip, Mlist, Glist, slist);
-    allTausCalc = vertcat(allTausCalc, instTauCalc.');
-end
-allTausCalc = allTausCalc(2:end, :);
-
-timeXCalc = zeros(size(allTausCalc, 1), 1);
-currTime = 0;
-interval = totalTime/size(allTausCalc, 1);
-for i=1:size(allTausCalc, 1)
-    timeXCalc(i) = currTime;
-    currTime = currTime + interval;
-end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Plot the data
-plot(timeXread, allTausReadings(:, 1), 'r-', 'LineWidth', 1.5);
-hold on;
-plot(timeXread, allTausReadings(:, 2), 'b-', 'LineWidth', 1.5);
-plot(timeXread, allTausReadings(:, 3), 'g-', 'LineWidth', 1.5);
-plot(timeXread, allTausReadings(:, 4), 'm-', 'LineWidth', 1.5);
-
-plot(timeXCalc, allTausCalc(:,1), 'r-.', 'LineWidth', 1.5);
-plot(timeXCalc, allTausCalc(:,2), 'b-.', 'LineWidth', 1.5);
-plot(timeXCalc, allTausCalc(:, 3), 'g-.', 'LineWidth', 1.5);
-plot(timeXCalc, allTausCalc(:, 4), 'm-.', 'LineWidth', 1.5);
-
-legend({'J1 Reading', 'J2 Reading', 'J3 Reading', 'J4 Reading', ...
-        'J1 Calc', 'J2 Calc', 'J3 Calc', 'J4 Calc'}, ...
-        'Location', 'best');
-
-xlabel('Time (s)');
-ylabel('Joint Torques (Nm)');
-title('Torque Readings vs Torque Calculations Traveling from A to B Using CP control');
-grid on;
-hold off;
+% % 
+% % % robot goes A to B and graph calculated torques from lspb/MR and another
+% % % from current readings and equation
+% % tic;
+% % % [allCurrentT1] = lspbVelocityControlAB(robot, thetasA, thetasB, thetasC, travelTime, speedRate);
+% % [j2current, allCurrentT1] = cpControl(robot, travelTime, thetasA, thetasB, currents, speedRate);
+% % % avgAllCurrT1 = [mean(allCurrentT1(:,1)), mean(allCurrentT1(:,2)), mean(allCurrentT1(:,3)), mean(allCurrentT1(:,4))];
+% % totalTime = toc;
+% % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % % current graph
+% % rowsCR = size(allCurrentT1, 1);
+% % timeXread = zeros(rowsCR, 1);
+% % currTime = 0;
+% % interval = totalTime/rowsCR;
+% % for i=1:rowsCR
+% %     timeXread(i) = currTime;
+% %     currTime = currTime + interval;
+% % end
+% % 
+% % figure;
+% % xlabel('Time (s)');
+% % ylabel('Torque (Nm)');
+% % %     title('Current at Joint 2 Velocity Control')
+% % % title(sprintf('Motor Current at Joint 2 at %.0f%% Speed (Velocity Control)', percentSpeed*100));
+% % allTausReadings = zeros(1,4);
+% % for i=1:size(allCurrentT1,1)
+% %     instTauR = coefficients(1).*allCurrentT1(i,:) + coefficients(2)*ones(1,4);
+% %     allTausReadings = vertcat(allTausReadings, instTauR);
+% % end
+% % allTausReadings = allTausReadings(2:end, :);
+% % 
+% % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % 
+% % m = 100; % number of points
+% % qAB1 = lspb(thetasA(1), thetasB(1), m);
+% % qAB2 = lspb(thetasA(2), thetasB(2), m);
+% % qAB3 = lspb(thetasA(3), thetasB(3), m);
+% % qAB4 = lspb(thetasA(4), thetasB(4), m);
+% % 
+% % qAB = [qAB1,qAB2,qAB3,qAB4];
+% % 
+% % allTausCalc = zeros(1,4);
+% % for i=1:size(qAB,1)
+% %     instTauCalc = 1000.*InverseDynamics(deg2rad(qAB(i,:).'), dthetalist, ddthetalist, g, Ftip, Mlist, Glist, slist);
+% %     allTausCalc = vertcat(allTausCalc, instTauCalc.');
+% % end
+% % allTausCalc = allTausCalc(2:end, :);
+% % 
+% % timeXCalc = zeros(size(allTausCalc, 1), 1);
+% % currTime = 0;
+% % interval = totalTime/size(allTausCalc, 1);
+% % for i=1:size(allTausCalc, 1)
+% %     timeXCalc(i) = currTime;
+% %     currTime = currTime + interval;
+% % end
+% % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % 
+% % % Plot the data
+% % plot(timeXread, allTausReadings(:, 1), 'r-', 'LineWidth', 1.5);
+% % hold on;
+% % plot(timeXread, allTausReadings(:, 2), 'b-', 'LineWidth', 1.5);
+% % plot(timeXread, allTausReadings(:, 3), 'g-', 'LineWidth', 1.5);
+% % plot(timeXread, allTausReadings(:, 4), 'm-', 'LineWidth', 1.5);
+% % 
+% % plot(timeXCalc, allTausCalc(:,1), 'r-.', 'LineWidth', 1.5);
+% % plot(timeXCalc, allTausCalc(:,2), 'b-.', 'LineWidth', 1.5);
+% % plot(timeXCalc, allTausCalc(:, 3), 'g-.', 'LineWidth', 1.5);
+% % plot(timeXCalc, allTausCalc(:, 4), 'm-.', 'LineWidth', 1.5);
+% % 
+% % legend({'J1 Reading', 'J2 Reading', 'J3 Reading', 'J4 Reading', ...
+% %         'J1 Calc', 'J2 Calc', 'J3 Calc', 'J4 Calc'}, ...
+% %         'Location', 'best');
+% % 
+% % xlabel('Time (s)');
+% % ylabel('Joint Torques (Nm)');
+% % title('Torque Readings vs Torque Calculations Traveling from A to B Using CP control');
+% % grid on;
+% % hold off;
 
 
 
